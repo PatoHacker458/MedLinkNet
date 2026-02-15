244f22f693835f6e7dcad2e132a4fbbc6f81ead1
midnai <patohacker458@gmail.com>
2026-02-15 15:51:16 -0600

    Name migration

--- backend/database.py
+++ backend/database.py
@@ -2,7 +2,9 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 import os
-DATABASE_URL = "postgresql://admin:secure_password_dev@h_saas_db/hospital_inventory"
+
+DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://admin:secure_password_dev@db:5432/hospital_inventory")
+
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
